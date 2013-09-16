4d34690562439a5f59f739823afce15c016c6462
Eugene Morozov <eugene.morozov@gmail.com>
2013-09-16 22:04:50 +0400

    Added missing import.

--- basic/blog/models.py
+++ basic/blog/models.py
@@ -1,5 +1,6 @@
 from django.utils.translation import ugettext_lazy as _
 from django.utils.safestring import mark_safe
+from django.utils.text import truncate_words
 from django.contrib.sites.models import Site
 from django.contrib.auth.models import User
 from django.db.models import permalink
